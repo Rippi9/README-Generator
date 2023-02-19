3b275d41788270ef503aa2d72ab83ef8361626f2
Rippi9 <swanssonben@hotmail.com>
2023-02-19 16:51:47 +1030

    added js and readme template

---
+++ readme-template.js
@@ -0,0 +1,37 @@
+//Contents to generate README file using parameters provided.
+const generateREADME = ({title, license, technologiesUsed, description, usage, installation, tests, contribution, gitHubUserName, userEmail}) =>
+`
+# ${title}
+![${license} License](https://img.shields.io/badge/license-${license}-blue.svg)
+${technologiesUsed}
+## Description
+${description}
+## Table of Contents
+* [Usage](#usage)
+* [Installation](#installation)
+* [Tests](#tests)
+* [License](#license)
+* [Contribution](#contribution)
+* [Questions](#questions)
+## Usage
+${usage}
+## Installation
+To install necessary dependencies, run the following command:
+\`\`\`
+${installation}
+\`\`\`
+## Tests
+To run tests, run the following command:
+\`\`\`
+${tests}
+\`\`\`
+## License
+The project is licensed under: ${license}.
+## Contribution
+${contribution}
+## Questions
+If you have any questions about the repository, open an issue or contact me directly at ${userEmail}. You can find more of my work at ${gitHubUserName}.
+`;
+
+//Exports this constant to be used in index.js file.
+module.exports = {generateREADME};
